f27ad66ef00f766e9875d7bfa1831e05875b7478
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-02-28 00:06:46 +0300

    Task-2

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,10 +1,3 @@
 #!/usr/bin/env ruby
 
-# Accept one argument from the command line and check if it matches the regex
-if ARGV.length == 1
-  test_string = ARGV[0]
-  puts test_string.match(/hbt+n/) ? "The test string '#{test_string}' matches the regular expression." : "The test string '#{test_string}' does not match the regular expression."
-else
-  puts "Please provide exactly one argument."
-end
-
+puts ARGV[0].scan(/hbt{2,5}n/).join
